a5a8ff09a26941784c556ae6634e87181be0f08c
Daniel García Jones <jones@Mac-mini-de-Daniel.local>
2023-04-05 14:00:05 +0200

    Adding config files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FoodplanModule } from './foodplan/foodplan.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     UserModule,
     FoodplanModule
   ],
